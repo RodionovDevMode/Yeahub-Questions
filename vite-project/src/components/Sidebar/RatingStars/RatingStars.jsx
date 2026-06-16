b5fc94da256fcd6411a5c8f041f29418b7673bc9
RodionovDevMode <rodionovdevmode@yandex.ru>
2026-06-16 21:08:07 +0300

    heat: Переделал фильтрацию вопросов добавив фильтрацию по сложности вопроса

--- vite-project/src/components/Sidebar/RatingStars/RatingStars.jsx
+++ vite-project/src/components/Sidebar/RatingStars/RatingStars.jsx
@@ -1,16 +1,28 @@
-function RatingStars() {
+import { useState } from 'react'
+import './RatingStart.css'
+
+function RatingStars({ onRatingChange }) {
+	const [selected, setSelected] = useState(null)
+	const numbersRating = [1, 2, 3, 4, 5]
+
 	return (
-		<>
+		<div className='rating-block'>
 			<h3>Рейтинг</h3>
-			<div className='stars'>
-				<span>★</span>
-				<span>★</span>
-				<span>★</span>
-				<span>★</span>
-				<span>☆</span>
-				<span>4</span>
+			<div className='rating-numbers'>
+				{numbersRating.map(num => (
+					<span
+						key={num}
+						className={`rating-number ${selected === num ? 'active' : ''}`}
+						onClick={() => {
+							setSelected(num)
+							onRatingChange(num)
+						}}
+					>
+						{num}
+					</span>
+				))}
 			</div>
-		</>
+		</div>
 	)
 }
 
