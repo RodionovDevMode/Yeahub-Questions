d8c3b5429c5ef2e43a3bbd414d1a0ce6544805a7
RodionovDevMode <rodionovdevmode@yandex.ru>
2026-06-16 10:38:56 +0300

    heat: Рефактор статуса диапозона сложности, выделение выбранного и сброс в дефол от повторного нажатия

--- vite-project/src/components/Sidebar/DifficultyTags/DifficultyTags.jsx
+++ vite-project/src/components/Sidebar/DifficultyTags/DifficultyTags.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
+import './DifficultyTags.css'
 
 function DifficultyTags({ onDifficultyChange }) {
 	const levels = ['1-3', '4-6', '7-8', '9-10']
 	const [internalLoading, setInternalLoading] = useState(false)
+	const [selectedLevel, setSelectedLevel] = useState(null)
 
 	const handleClick = level => {
 		if (internalLoading) return
 		setInternalLoading(true)
 		setTimeout(() => {
-			onDifficultyChange(level)
+			const newLevel = level === selectedLevel ? null : level
+			setSelectedLevel(newLevel)
+			onDifficultyChange(newLevel)
 			setInternalLoading(false)
-		}, 5000)
+		}, 1000)
 	}
 
 	return (
@@ -18,8 +22,12 @@ function DifficultyTags({ onDifficultyChange }) {
 			<h3>Уровень сложности</h3>
 			<div className='tags'>
 				{levels.map(level => (
-					<button key={level} onClick={() => handleClick(level)}>
-						{internalLoading ? <p>Идёт загрузка...</p> : level}
+					<button
+						key={level}
+						onClick={() => handleClick(level)}
+						className={level === selectedLevel ? 'active' : null}
+					>
+						{internalLoading ? 'Идёт загрузка...' : level}
 					</button>
 				))}
 			</div>
